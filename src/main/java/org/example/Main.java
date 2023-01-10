a13a581af1ac30ceed026db93d42da426f5a6ee7
ssongis <thddltmf97@naver.com>
2023-01-10 15:50:19 +0900

    #2023_01_10_상속예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 import org.example.basic.Dancer;
+import org.example.inherit.IdolDancer;
+import org.example.inherit.StreetDancer;
 
 public class Main {
     public static void main(String[] args) {
-        Dancer dancer = new Dancer("총알탄소년단", "춤신춤왕");
-
-        //dancer.stretch();
-        //dancer.makeFlexible();
-        dancer.dance();
+        IdolDancer idolDancer = new IdolDancer("AB6Ix", "이대휘");
+        StreetDancer streetDancer = new StreetDancer("길거리춤꾼", "박격포");
+        idolDancer.dance();
+        System.out.println();
+        streetDancer.dance();
+        //new IdolDancer( "AB6IX", "이대휘");
     }
 }
