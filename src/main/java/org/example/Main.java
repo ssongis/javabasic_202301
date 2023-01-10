c48da7060a1ab0150ddcdcde6a38c2512f5a7e63
ssongis <thddltmf97@naver.com>
2023-01-10 15:09:42 +0900

    #2023_01_10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
+import org.example.basic.Dancer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요!");
+        Dancer dancer = new Dancer("총알탄소년단", "춤신춤왕");
+
+        //dancer.stretch();
+        //dancer.makeFlexible();
+        dancer.dance();
     }
 }
